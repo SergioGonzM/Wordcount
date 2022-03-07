6d21d1c901817ed04c3a8267d031f692b73fc55b
SergioGonzM <sgonzalezm03@hotmail.com>
2022-03-06 23:56:55 -0600

    improve legibility

--- lib/word_count.ex
+++ lib/word_count.ex
@@ -1,7 +1,7 @@
 defmodule Wordcount do
     def count(var) do
       {:ok, text} = File.read(var)
-      text |> String.normalize(:nfd) |> String.replace(~r/[^0-9A-z\s]/u, "") |> String.downcase() |> String.trim() |> String.split()
+      text |> String.downcase() |> String.normalize(:nfd) |> String.replace(~r/[^0-9A-z\s]/u, "") |> String.split()
       |> Enum.frequencies()
     end
 end
