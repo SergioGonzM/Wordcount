3287a79dff1faa5b0e3f3c8a09f0befa971fad20
SergioGonzM <sgonzalezm03@hotmail.com>
2022-03-07 21:50:12 -0600

    Fix: Wordcount

--- lib/word_count.ex
+++ lib/word_count.ex
@@ -2,7 +2,8 @@ defmodule Wordcount do
     def count(var) do
       {:ok, text} = File.read(var)
       text |> String.downcase() |> String.normalize(:nfd) |> String.replace(~r/[^0-9A-z\s]/u, "") |> String.split()
-      |> Enum.frequencies()
+      |> Enum.group_by(fn(x) -> x end)
+      |> Enum.reduce(%{}, fn {k, v}, acc -> Map.put(acc, k, Enum.count(v)) end)
     end
 end
 
