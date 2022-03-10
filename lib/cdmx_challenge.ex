ef294dd0dc450bd9f2b550779d092d399fbf0fce
SergioGonzM <sgonzalezm03@hotmail.com>
2022-03-09 22:33:07 -0600

    Add: function metro_lines

---
+++ lib/cdmx_challenge.ex
@@ -0,0 +1,61 @@
+defmodule MetroCdmxChallenge do
+  import SweetXml
+  @doc """
+  Obtains ....
+  
+   ## Examples
+     iex> MetroCdmxChallenge.metro_lines("./data/tiny_metro.kml")
+    [
+       %{name: "Línea 5", stations:
+         [
+           %{name: "Pantitlan", coords: "90.0123113 30.012121"},
+          %{name: "Hangares", coords: "90.0123463 30.012158"},
+         ]
+      },
+       %{name: "Línea 3", stations:
+         [
+           %{name: "Universidad", coords: "90.0123113 30.012121"},
+          %{name: "Copilco", coords: "90.0123463 30.012158"},
+         ]
+      }
+    ]
+  """
+  defmodule Line do
+    #defstruct name: "", stations:[] # con valor default para cada atributo
+     defstruct [:name, :stations]
+  end
+  
+  defmodule Station do
+     defstruct [:name, :coords]
+  end
+
+  def metro_graph(xml_path) do
+     #lines = metro_lines(xml_path)
+     # Create graph ...
+  end
+
+  def metro_lines(xml_path) do
+      doc = File.read!(xml_path)
+      nombre_lineas = doc |> xpath(~x"//Document/Folder[1]/Placemark/name/text()"l) |> Enum.map(fn l ->List.to_string(l) |> String.split(" ") |> Enum.at(1) end)
+      estaciones_individuales = doc |> xpath(~x"//Document/Folder[2]/Placemark"l,
+      name: ~x"./name/text()",
+      line: ~x"./description/text()", 
+      coords: ~x"./Point/coordinates/text()"
+    )
+     |> Enum.map(fn x -> %{name: to_string(x.name), line: to_string(x.line) |> String.split(".") |> Enum.at(0) |> String.split(" ") |> Enum.at(1), coords: to_string(x.coords) |> String.replace("\n", "")} end)
+
+       estaciones = Enum.map(nombre_lineas, fn y -> Enum.filter(estaciones_individuales, fn z -> z.line == y end) |> Enum.map(fn st -> 
+         %Station{
+          name: st.name,
+          coords: st.coords
+        }end)
+      end)
+
+      Enum.map(0..12, fn o -> 
+        %Line{
+          name: "Linea #{Enum.at(nombre_lineas, o)}",
+          stations: Enum.at(estaciones,o)
+        }
+      end)
+  end 
+end
