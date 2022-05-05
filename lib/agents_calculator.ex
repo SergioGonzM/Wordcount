0d02a28382f35f0291c47ee5db8c0a4c04f5b281
SergioGonzM <sgonzalezm03@hotmail.com>
2022-05-05 15:06:13 -0500

    Calculator with agents

---
+++ lib/agents_calculator.ex
@@ -0,0 +1,17 @@
+defmodule CalculatorAgent do
+    use Agent
+
+    def init(init_value) do
+      Agent.start(fn -> init_value end)
+    end
+  
+    def sum(pid, val), do: Agent.update(pid, fn state -> state + val end)
+    def sub(pid, val), do: Agent.update(pid, fn state -> state - val end)
+    def mult(pid, val), do: Agent.update(pid, fn state -> state * val end)
+    def div(pid, val), do: Agent.update(pid, fn state -> state / val end)
+  
+    def state(pid) do
+        Agent.get(pid, fn value -> value end)
+    end
+
+  end
