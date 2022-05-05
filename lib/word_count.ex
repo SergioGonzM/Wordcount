7270581dd8f27e55aaa517d6e203cd69e303bec6
SergioGonzM <sgonzalezm03@hotmail.com>
2022-05-04 19:39:21 -0500

    Word count with tasks

--- lib/word_count.ex
+++ lib/word_count.ex
@@ -1,9 +1,25 @@
 defmodule Wordcount do
+
     def count(var) do
-      {:ok, text} = File.read(var)
-      text |> String.downcase() |> String.normalize(:nfd) |> String.replace(~r/[^0-9A-z\s]/u, "") |> String.split()
+      String.normalize(var, :nfd) 
+      |> String.replace(~r/[^0-9A-z\s]/u, "") 
+      |> String.downcase()
+      |> String.split()
       |> Enum.group_by(fn(x) -> x end)
       |> Enum.reduce(%{}, fn {k, v}, acc -> Map.put(acc, k, Enum.count(v)) end)
+    end
+
+    def frequencies_tasks(path) do
+      path
+      |> File.stream!()
+      |> Enum.chunk_every(1_000)
+      |> Enum.map(fn lines -> Task.async(fn -> count(Enum.join(lines)) end) end)
+      |> Enum.map(fn task -> Task.await(task) end)
+    end
+
+    def frequencies(path) do
+      path |> File.read!()
+      |> count()
     end
 end
 
